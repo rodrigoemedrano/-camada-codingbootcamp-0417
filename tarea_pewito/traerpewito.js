db01a9a7275202a2cad6cb9061072763584cef98
Rodrigo Medrano <rodrigoezequielmedrano@gmail.com>
2020-10-27 17:56:23 -0300

    tarea pewito

---
+++ tarea_pewito/traerpewito.js
@@ -0,0 +1,34 @@
+const botoncitoLindo = document.getElementById("button");
+const imagenContainer = document.getElementById("imagen");
+
+window.addEventListener("DOMContentLoaded", renderizar);
+botoncitoLindo.addEventListener("click", renderizar);
+
+const traerPewito = async () => {
+  try {
+    const fetchPewito = await fetch("https://dog.ceo/api/breeds/image/random");
+    const pewitoDefinitivo = await fetchPewito.json();
+    return pewitoDefinitivo.message;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const mostrarPerrito = (imagenUrl) => {
+  const codigoHTML = `<img src="${imagenUrl}">`;
+  imagenContainer.innerHTML = codigoHTML;
+};
+
+async function renderizar() {
+    const imageUrl = await traerPewito();
+    mostrarPerrito(imageUrl);
+};
+
+
+// **************************************** TAREA PARA EL HOGAR ****************************************
+// Hacer la funcion del botón
+// Quiero que cuando haga click en el botón:
+// Vaya a buscar un link random
+// Y luego lo pinte (renderice)
+// Esto ya lo estamos haciendo en algún lado...🤔
+// PERO EN DONDE??!! 😩😩😩😩😩😩
