e813b7dcc25873f6c38caa0e8ae0abc28e43f636
Rodrigo <rodrigoezequielmedrano@gmail.com>
2020-10-29 15:48:31 -0300

    css pokemon

--- tareaPokemon/pokemon.js
+++ tareaPokemon/pokemon.js
@@ -1,37 +1,31 @@
-const imagenContainer = document.getElementById("imagen");
+const cardContainer = document.getElementById("cardContainer");
 
 
 
-const traerPokemon = async () => {
+const traerPokemon = async() => {
     try {
-      const fetchPokemon = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=20");
-      const pokemonDefinitivo = await fetchPokemon.json();
-       
-      pokemonDefinitivo.results.map( async function(valor) {
-        const urlDefinitiva = await fetch(valor.url);
-        const jsonUrl = await urlDefinitiva.json();
-        const nombre = jsonUrl.name
-        const id = jsonUrl.id
-        pintarPokemon(nombre, id)
+        const fetchPokemon = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=20");
+        const pokemonDefinitivo = await fetchPokemon.json();
 
-      }) 
+        pokemonDefinitivo.results.map(async function(valor) {
+            const urlDefinitiva = await fetch(valor.url);
+            const jsonUrl = await urlDefinitiva.json();
+            const nombre = jsonUrl.name.toUpperCase();
+            const id = jsonUrl.id;
+            pintarPokemon(nombre, id);
+        })
 
 
     } catch (error) {
-      console.log(error);
+        console.log(error);
     }
-    
-  };
+
+};
 
 traerPokemon()
 
 const pintarPokemon = (nombre, id) => {
-    const codigoHTML = `<p class="parrafito">${nombre}</p> <img class="imagen" src="https://pokeres.bastionbot.org/images/pokemon/${id}.png"> `;
-    imagenContainer.innerHTML += codigoHTML;
+    const codigoHTML = `<div class="card"><p class="titulo">${nombre}</p> <img class="imagen" src="https://pokeres.bastionbot.org/images/pokemon/${id}.png"></div>`;
+    cardContainer.innerHTML += codigoHTML;
 
-  };
-
-
-
-  
-
+};
